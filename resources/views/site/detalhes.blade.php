288697a335d905275f36430a107b4d26cec71156
laudringarralaga <laudrin.garralaga@outlook.com>
2019-05-06 14:18:10 -0300

    modificações na view produtos 4

--- resources/views/site/detalhes.blade.php
+++ resources/views/site/detalhes.blade.php
@@ -27,17 +27,17 @@
             <div class="col-md-6">
               <div class="box">
                 <h1 class="text-center" style="font-size: 15pt">{{$prod->nome}} - <span class="codigo" style="color: red">CÓD. {{$prod->codigo}}</span></h1>
-                  @if (empty($produto->val_avista_ata) && $produto->codigo != "3254/3255/3256")
-                      <p class="card-text-2" style="margin-bottom: 0">R${{$produto->val_parcelado_un}} EM ATÉ 3X SEM JUROS ou</p>                     
-                      <p class="card-text" style="margin-bottom: 95px">R${{$produto->val_avista_un}} À VISTA <br> (10% de desconto)</p>    
-                  @elseif ($produto->codigo == "3254/3255/3256")
-                      <p class="card-text-2" style="margin-bottom: 0">R${{$produto->val_parcelado_un}} O PAR EM ATÉ 3X SEM JUROS ou</p>                     
-                      <p class="card-text" style="margin-bottom: 95px">R${{$produto->val_avista_un}} O PAR À VISTA <br> (10% de desconto)</p>
+                  @if (empty($prod->val_avista_ata) && $prod->codigo != "3254/3255/3256")
+                      <p class="card-text-2" style="margin-bottom: 0">R${{$prod->val_parcelado_un}} EM ATÉ 3X SEM JUROS ou</p>                     
+                      <p class="card-text" style="margin-bottom: 95px">R${{$prod->val_avista_un}} À VISTA <br> (10% de desconto)</p>    
+                  @elseif ($prod->codigo == "3254/3255/3256")
+                      <p class="card-text-2" style="margin-bottom: 0">R${{$prod->val_parcelado_un}} O PAR EM ATÉ 3X SEM JUROS ou</p>                     
+                      <p class="card-text" style="margin-bottom: 95px">R${{$prod->val_avista_un}} O PAR À VISTA <br> (10% de desconto)</p>
                   @else
-                      <p class="card-text-2" style="margin-bottom: 0">R${{$produto->val_parcelado_ata}} O PACOTE EM ATÉ 3X SEM JUROS ou</p>                     
-                      <p class="card-text" style="margin-bottom: 5px">R${{$produto->val_avista_ata}} O PACOTE À VISTA <br> (10% de desconto)</p>    
-                      <p class="card-text-2" style="margin-bottom: 0">R${{$produto->val_parcelado_un}} A UN. EM ATÉ 3X SEM JUROS ou</p>                     
-                      <p class="card-text" style="margin-bottom: 10px">R${{$produto->val_avista_un}} A UN. À VISTA <br> (10% de desconto)</p> 
+                      <p class="card-text-2" style="margin-bottom: 0">R${{$prod->val_parcelado_ata}} O PACOTE EM ATÉ 3X SEM JUROS ou</p>                     
+                      <p class="card-text" style="margin-bottom: 5px">R${{$prod->val_avista_ata}} O PACOTE À VISTA <br> (10% de desconto)</p>    
+                      <p class="card-text-2" style="margin-bottom: 0">R${{$prod->val_parcelado_un}} A UN. EM ATÉ 3X SEM JUROS ou</p>                     
+                      <p class="card-text" style="margin-bottom: 10px">R${{$prod->val_avista_un}} A UN. À VISTA <br> (10% de desconto)</p> 
                   @endif
               </div>
             </div>
